0ada5541b1fd11e0127b1057eca97311018cae0c
Marcel Ferry <marcelferry@gmail.com>
2025-01-02 22:21:22 -0300

    New adjust in st7735 driver

--- core/disp_drivers/ST7735/ST7735.py
+++ core/disp_drivers/ST7735/ST7735.py
@@ -4,6 +4,55 @@ import core.disp_drivers.ST7735.config as config
 
 class ST7735(config.RaspberryPi):
 
+    NOP = 0x0
+    SWRESET = 0x01
+    RDDID = 0x04
+    RDDST = 0x09
+
+    SLPIN  = 0x10
+    SLPOUT  = 0x11
+    PTLON  = 0x12
+    NORON  = 0x13
+
+    INVOFF = 0x20
+    INVON = 0x21
+    DISPOFF = 0x28
+    DISPON = 0x29
+    CASET = 0x2A
+    RASET = 0x2B
+    RAMWR = 0x2C
+    RAMRD = 0x2E
+
+    VSCRDEF = 0x33
+    VSCSAD = 0x37
+
+    COLMOD = 0x3A
+    MADCTL = 0x36
+
+    FRMCTR1 = 0xB1
+    FRMCTR2 = 0xB2
+    FRMCTR3 = 0xB3
+    INVCTR = 0xB4
+    DISSET5 = 0xB6
+
+    PWCTR1 = 0xC0
+    PWCTR2 = 0xC1
+    PWCTR3 = 0xC2
+    PWCTR4 = 0xC3
+    PWCTR5 = 0xC4
+    VMCTR1 = 0xC5
+
+    RDID1 = 0xDA
+    RDID2 = 0xDB
+    RDID3 = 0xDC
+    RDID4 = 0xDD
+
+    PWCTR6 = 0xFC
+
+    GMCTRP1 = 0xE0
+    GMCTRN1 = 0xE1
+
+
     width = 128
     height = 128 
 
@@ -60,60 +109,72 @@ class ST7735(config.RaspberryPi):
             self.LCD_Y_Adjust = config.LCD_Y
 
         # Set the read / write scan direction of the frame memory
-        self.command(0x36)		#MX, MY, RGB mode 
+        self.command(ST7735.MADCTL)		#MX, MY, RGB mode 
         self.data( MemoryAccessReg_Data | 0x08)	#0x08 set RGB
 
     def Init(self):
         self.module_init()
         self.reset()
 
+        #65k mode
+        self.command(ST7735.COLMOD)
+        self.data(0x05) #16 bit color.
+
         #ST7735R Frame Rate
-        self.command(0xB1)
-        self.data(0x01)
-        self.data(0x2C)
+        #Frame rate control.
+        self.command(ST7735.FRMCTR1)
+        #fastest refresh, 6 lines front, 3 lines back.
+        self.data(ST7735.SWRESET)
+        self.data(ST7735.RAMWR)
         self.data(0x2D)
 
-        self.command(0xB2)
-        self.data(0x01)
-        self.data(0x2C)
+        #Frame rate control.
+        self.command(ST7735.FRMCTR2) 
+        #fastest refresh, 6 lines front, 3 lines back.
+        self.data(ST7735.SWRESET)
+        self.data(ST7735.RAMWR)
         self.data(0x2D)
 
-        self.command(0xB3)
-        self.data(0x01)
-        self.data(0x2C)
+        #Frame rate control.
+        self.command(ST7735.FRMCTR3) 
+        #fastest refresh, 6 lines front, 3 lines back.
+        self.data(ST7735.SWRESET)
+        self.data(ST7735.RAMWR)
         self.data(0x2D)
-        self.data(0x01)
-        self.data(0x2C)
+        self.data(ST7735.SWRESET)
+        self.data(ST7735.RAMWR)
         self.data(0x2D)
 
-        #Column inversion 
-        self.command(0xB4)
-        self.data(0x07)
+        # Display inversion control
+        self.command(ST7735.INVCTR)
+        self.data(0x07) #Line inversion.
 
         #ST7735R Power Sequence
-        self.command(0xC0)
-        self.data(0xA2)
+        self.command(ST7735.PWCTR1)
+        self.data(0xA2) 
         self.data(0x02)
         self.data(0x84)
-        self.command(0xC1)
-        self.data(0xC5)
 
-        self.command(0xC2)
-        self.data(0x0A)
-        self.data(0x00)
+        self.command(ST7735.PWCTR2)
+        self.data(0xC5) #VGH = 14.7V, VGL = -7.35V
 
-        self.command(0xC3)
-        self.data(0x8A)
-        self.data(0x2A)
-        self.command(0xC4)
-        self.data(0x8A)
-        self.data(0xEE)
+        self.command(ST7735.PWCTR3)
+        self.data(0x0A) #Opamp current small
+        self.data(0x00) #Boost frequency
 
-        self.command(0xC5) #VCOM 
+        self.command(ST7735.PWCTR4)
+        self.data(0x8A) #Opamp current small
+        self.data(0x2A) #Boost frequency
+
+        self.command(ST7735.PWCTR5)
+        self.data(0x8A) #Opamp current small
+        self.data(0xEE) #Boost frequency
+
+        self.command(ST7735.VMCTR1) #VCOM 
         self.data(0x0E)
 
         #ST7735R Gamma Sequence
-        self.command(0xe0)
+        self.command(ST7735.GMCTRP1)
         self.data(0x0f)
         self.data(0x1a)
         self.data(0x0f)
@@ -131,7 +192,7 @@ class ST7735(config.RaspberryPi):
         self.data(0x02)
         self.data(0x10)
 
-        self.command(0xe1)
+        self.command(ST7735.GMCTRN1)
         self.data(0x0f)
         self.data(0x1b)
         self.data(0x0f)
@@ -149,41 +210,37 @@ class ST7735(config.RaspberryPi):
         self.data(0x03)
         self.data(0x10)
 
-	#Enable test command
-        self.command(0xF0)
-        self.data(0x01)
+	    #Enable test command
+        #self.command(0xF0)
+        #self.data(ST7735.SWRESET)
 
         #Disable ram power save mode
-        self.command(0xF6)
-        self.data(0x00)
-
-        #65k mode
-        self.command(0x3A)
-        self.data(0x05)
+        #self.command(0xF6)
+        #self.data(0x00)
 
         self.SetGramScanWay(config.SCAN_DIR_DFT)
 
-        self.command(0x11)
+        #self.command(0x11)
 
-        self.command(0x29)
+        self.command(ST7735.DISPON)
 
 
     def SetWindows(self, Xstart, Ystart, Xend, Yend):
         #set the X coordinates
-        self.command(0x2A)
+        self.command(ST7735.CASET)
         self.data(0x00)               #Set the horizontal starting point to the high octet
         self.data((Xstart & 0xff) + self.LCD_X_Adjust)      #Set the horizontal starting point to the low octet
         self.data(0x00)               #Set the horizontal end to the high octet
         self.data(((Xend - 1) & 0xff) + self.LCD_X_Adjust) #Set the horizontal end to the low octet 
         
         #set the Y coordinates
-        self.command(0x2B)
+        self.command(ST7735.RASET)
         self.data(0x00)
         self.data((Ystart & 0xff)  + self.LCD_Y_Adjust)
         self.data(0x00)
         self.data(((Yend - 1) & 0xff ) + self.LCD_Y_Adjust)
 
-        self.command(0x2C) 
+        self.command(ST7735.RAMWR) 
         
     def ShowImage(self,Image):
         """Set buffer to value of Python Imaging Library image."""
--- plugins/NFC/nfc.py
+++ plugins/NFC/nfc.py
@@ -3,6 +3,8 @@ import core.pn532.reader as reader
 from core.plugin import BasePwnhyvePlugin
 from core.utils import config
 
+# https://github.com/gassajor000/pn532pi
+
 card_reader = reader.pPN532()
 
 class PWN_Nfc(BasePwnhyvePlugin):
